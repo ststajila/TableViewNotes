625813e3000354d85b00e97313aab0a3d3d8881d
Stanislav <stazilastas@gmail.com>
2023-10-18 09:07:41 -0500

    Initial Commit

---
+++ TableViewNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewNotes
+//
+//  Created by STANISLAV STAJILA on 10/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
